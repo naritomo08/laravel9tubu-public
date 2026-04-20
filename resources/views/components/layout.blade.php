6ddd80a7ddd5cf51312a059af3de4fde5543c171
naritomo08 <naritomo08@gmail.com>
2026-04-20 16:05:11 +0900

    upload

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,8 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
-    <script src="{{ asset('/js/tweet-refresh.js') }}?v={{ filemtime(public_path('js/tweet-refresh.js')) }}" defer></script>
-    <script src="{{ asset('/js/like.js') }}?v={{ filemtime(public_path('js/like.js')) }}" defer></script>
     <script src="{{ mix('/js/app.js') }}" defer></script>
     <title>{{ $title ?? 'つぶやきアプリ' }}</title>
     @stack('css')
